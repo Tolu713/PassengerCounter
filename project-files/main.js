0e08905da6629852614a80d1d1897c44d07b836f
Tolu713 <104229820+Tolu713@users.noreply.github.com>
2022-05-07 10:23:34 -0700

    added stuff

--- project-files/main.js
+++ project-files/main.js
@@ -1,14 +1,21 @@
 
 
 // initialize count var
-var count = 0;
+let count = 0;
 // onbuttonclcick count++
-var counter = document.getElementById("counting")
+let counter = document.getElementById("counting")
 function increment() {
     count += 1
     counter.innerText = count
 }
 
+function save() {
+    console.log(count)
+}
+let nname = "Tolu"
+let greeting = "Yo, my name is "
+let myGreeting = greeting + nname
+console.log(myGreeting)
 
 
 
