7e2c7f715206857b3ea45df453d34b2dad1209e1
Tolu713 <104229820+Tolu713@users.noreply.github.com>
2022-05-06 21:59:01 -0700

    changes

--- project-files/main.js
+++ project-files/main.js
@@ -1,1 +1,15 @@
+//document.getElementById("counting").innerText = 3
 
+var count = 0
+var myAge = 11
+
+let dogRatio = 7
+
+let bonusPoints = 50
+bonusPoints *= 2
+bonusPoints /= 4
+bonusPoints += 45
+
+var myDogAge = myAge * dogRatio
+console.log(myDogAge)
+console.log(bonusPoints)
