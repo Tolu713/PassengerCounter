0fea0432a153ab90899bfd318dd36468ccdeeb2a
Tolu713 <104229820+Tolu713@users.noreply.github.com>
2022-05-08 12:42:56 -0700

    polishing save button

--- project-files/main.js
+++ project-files/main.js
@@ -2,16 +2,19 @@
 
 // initialize count var
 let count = 0;
+let countTwo = 0;
 // onbuttonclcick count++
 let counter = document.getElementById("counting")
 let prevEnt = document.getElementById("prev-ent")
 function increment() {
     count += 1
     counter.innerText = count
+    countTwo += 1
 }
 
 function save() {
-    prevEnt.innerText += " " + count + " " + "-"
+    prevEnt.innerText += " " + countTwo + " " + "-"
+    countTwo = 0 
 }
 
 
