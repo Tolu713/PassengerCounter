631332ccf4de321e57e64ab71e95135e4a044580
Tolu713 <104229820+Tolu713@users.noreply.github.com>
2022-05-08 13:16:12 -0700

    polishing

--- project-files/main.js
+++ project-files/main.js
@@ -6,15 +6,19 @@ let countTwo = 0;
 // onbuttonclcick count++
 let counter = document.getElementById("counting")
 let prevEnt = document.getElementById("prev-ent")
+let countingBatch = document.getElementById("countingBatch")
+
 function increment() {
     count += 1
     counter.innerText = count
     countTwo += 1
+    countingBatch.innerText = countTwo
 }
 
 function save() {
     prevEnt.innerText += " " + countTwo + " " + "-"
     countTwo = 0 
+    countingBatch.innerText = 0
 }
 
 
